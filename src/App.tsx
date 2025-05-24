4bb0c8c558f59a5e1e7d5cf5ab492009a3a30d01
Fernando Matos <fernandojesusmatos123@gmail.com>
2025-05-24 14:55:46 -0300

    Precisa de Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { TaskStateModel } from "./models/TaskStateModel";
 import { Home } from "./pages/Home";
 
 import "./styles/global.css";
 import "./styles/theme.css";
+import { useState } from "react";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formatttedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 
 export function App() {
+  const [state, setState] = useState(initialState);
+
   return <Home />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,21 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
+
+
 
 export function Home() {
   return (
     <MainTemplate>
-        <Container>
-            <CountDown/>
-        </Container>
+      <Container>
+        <CountDown />
+      </Container>
 
-        <Container>
-            <MainForm/>
-        </Container>
+      <Container>
+        <MainForm />
+      </Container>
     </MainTemplate>
   );
 }
